85b3060e7e5fc027683334cf8d908d6fdf630f56
r.ostapchuk <kucheriavii@gmail.com>
2023-12-25 21:43:32 +0200

    21:47 Variablrs

---
+++ variables.R
@@ -0,0 +1,20 @@
+#почистити створені змінні
+rm(list = ls()) 
+
+model1 <- 'hello' #створити змінну
+typeof(model1)
+class(model1)
+model1
+
+hello_string <- 'hello'
+hello_string <- "real hello"
+hello_string
+
+.pairs <-100
+.pairs
+
+#правила такі ж як і в інших мовах. Можна починати з крапки. Оператор присвоєння <-
+x <- 100
+typeof(x)
+y <- 100L #літера L перетворює в integer
+typeof(y)
