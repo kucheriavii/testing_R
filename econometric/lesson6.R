a1ef9ad853591a7649288cbb2af5bf943f130c59
r.ostapchuk <kucheriavii@gmail.com>
2023-12-27 23:29:46 +0200

    lesson 6 24:00

---
+++ econometric/lesson6.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+cars <- read_delim('https://raw.githubusercontent.com/Aranaur/datasets/main/datasets/cars2018.csv', delim = ';')
+
+#xlsm
+library(readxl)
+
+cars_xl_list1 = read_excel("00_data/cars2018.xlsx")
+cars_xl_list2 = read_excel("00_data/cars2018.xlsx", "list2")
+
+sheets = excel_sheets("00_data/cars2018.xlsx");
+cars_xl_test = read_excel("00_data/cars2018.xlsx", sheets[3])
+
+glimpse(cars)
+
+filter(cars, transmission == 'Automatic', gears==10)
+cars_disp_cyl <- filter(cars, displacement > 60, cylinders==12)
