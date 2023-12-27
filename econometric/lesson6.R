b4837048adb790c70312ea7d563ccfc5b474546e
r.ostapchuk <kucheriavii@gmail.com>
2023-12-27 23:40:30 +0200

    lesson 6 32:00

--- econometric/lesson6.R
+++ econometric/lesson6.R
@@ -14,3 +14,9 @@ glimpse(cars)
 
 filter(cars, transmission == 'Automatic', gears==10)
 cars_disp_cyl <- filter(cars, displacement > 60, cylinders==12)
+
+cars_rec <- filter(cars, cylinders == 4 | cylinders == 6)
+
+cars_in <- filter(cars, cylinders %in% c(3:5))
+
+filter(cars, str_detect(model, "Audi"))
