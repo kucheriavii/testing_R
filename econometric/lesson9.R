9db9183f9a11b0731344eec47e44defdf908a26b
r.ostapchuk <kucheriavii@gmail.com>
2023-12-29 22:51:12 +0200

    lesson 9

--- econometric/lesson9.R
+++ econometric/lesson9.R
@@ -9,4 +9,44 @@ mtcars %>%
   ) +
   geom_point(color = "blue")
 
-mtcars
+mtcars %>% ggplot(aes(mpg, disp)) + 
+  geom_point(size=2, shape=1)
+
+
+mtcars %>% ggplot(aes(mpg, disp)) + 
+  geom_point()+
+  geom_line()
+
+mtcars %>% 
+  ggplot(aes(mpg, disp)) + 
+  geom_point(aes(color = as.factor(cyl), shape = as.factor(vs), size = 3))
+
+mtcars %>%
+  filter(cyl==8)
+
+#manual colors
+mtcars %>% 
+  ggplot(aes(mpg, disp)) + 
+  geom_point(aes(color = as.factor(cyl), shape = as.factor(vs), size = 3))+
+  scale_color_manual(values = c("#ff0000", "#009999", "#220000"))
+
+
+mtcars %>% 
+  ggplot(aes(mpg, disp)) + 
+  geom_point(aes(color = as.factor(cyl), shape = as.factor(vs), size = 3)) +
+  scale_color_brewer(palette = 'Set1')
+
+
+
+mtcars %>% 
+  ggplot(aes(mpg, disp)) + 
+  geom_point(aes(color = as.factor(cyl), shape = as.factor(vs), size = 3))+
+  theme_void()+
+  theme(legend.position = 'none')+
+  theme(plot.background = element_rect(fill = "#A08000"))
+
+
+mtcars %>% 
+  ggplot(aes(disp, mpg, shape=as.factor(cyl))) +
+  geom_point(size=3)+
+  geom_smooth(method = 'lm', se=FALSE)
